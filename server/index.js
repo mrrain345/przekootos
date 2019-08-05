b93601a4ca5e8fc1ab6b0bb77391ccdea96e9822
mrrain345 <mc.rain345@gmail.com>
2019-08-05 15:22:45 +0200

    add postgres

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const { Client } = require('pg');
 
 const server = express();
 const port = process.env.PORT || 3000;
 
-server.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world'
-  });
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'qwerty',
+  port: 5432,
+});
+
+server.get('/', async (req, res) => {
+  await client.connect();
+  const db = await client.query('SELECT * FROM users');
+  const output = db.rows[0];
+  await client.end();
+  res.json(output);
 });
 
 server.listen(port, () => {
