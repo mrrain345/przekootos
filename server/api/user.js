4cee1c8d7bec2e1c6d94b95470fcc8045975cc41
mrrain345 <mc.rain345@gmail.com>
2019-08-07 13:44:25 +0200

    add 'Register' functionality

--- server/api/user.js
+++ server/api/user.js
@@ -7,7 +7,7 @@ module.exports = (server, db) => {
     const query = await db.query('SELECT * FROM users');
     res.json(query.rows);
   });
-  
+
   // get user
   server.get('/api/user/:id', async (req, res) => {
     const query = await db.query('SELECT * FROM users WHERE id=$1', [req.params.id]);
@@ -16,46 +16,56 @@ module.exports = (server, db) => {
 
   // add new user
   server.post('/api/user', async (req, res) => {
-    // { username, password, confirm_password, email }
-    if (!req.body.username) {
-      res.json({ code: 1, message: 'Username is required' });
-      return;
+    // { fname, lname, password, cpassword, email }
+    let errors = [];
+
+    if (!req.body.fname) {
+      errors.push({ code: 1, target: 'fname', message: 'First name is required' });
     }
 
-    if (req.body.username.length < 3) {
-      res.json({ code: 2, message: 'Username is too short (min: 3 characters)' });
-      return;
-    }
-
-    if (!req.body.password) {
-      res.json({ code: 3, message: 'Password is required' });
-      return;
-    }
-
-    if (req.body.password.length < 8) {
-      res.json({ code: 4, message: 'Password is too short (min 8 characters)' });
-      return;
-    }
-
-    if (req.body.password !== req.body.confirm_password) {
-      res.json({ code: 5, message: 'Passwords do not match' });
-      return;
+    if (!req.body.lname) {
+      errors.push({ code: 2, target: 'lname', message: 'Last name is required' });
     }
 
     if (!req.body.email) {
-      res.json({ code: 6, message: 'Email is required' });
-      return;
+      errors.push({ code: 3, target: 'email', message: 'Email is required' });
+    }
+    else if (!validator.isEmail(req.body.email)) {
+      errors.push({ code: 4, target: 'email', message: 'Email is not correct' });
     }
 
-    if (!validator.isEmail(req.body.email)) {
-      res.json({ code: 7, message: 'Email is not correct' });
-      return;
+    if (!req.body.password) {
+      errors.push({ code: 5, target: 'password', message: 'Password is required' });
+    }
+    else if (req.body.password.length < 8) {
+      errors.push({ code: 6, target: 'password', message: 'Password is too short (min 8 characters)' });
     }
 
-    const query = await db.query(
-      'INSERT INTO users (username, password, email) VALUES ($1, $2, $3)',
-      [req.body.username, hash.generate(req.body.password), req.body.email]
+    if (req.body.password !== req.body.cpassword) {
+      errors.push({ code: 7, target: 'cpassword', message: 'Passwords do not match' });
+    }
+
+    const email = await db.query(
+      'SELECT id FROM users WHERE email=$1 LIMIT 1',
+      [req.body.email],
     );
-    res.json({ code: 0, message: 'success' });
+
+    if (email.rows.length !== 0) {
+      errors.push({ code: 8, target: 'email', message: 'Email is used' });
+    }
+
+    const ok = errors.length === 0;
+
+    if (ok) {
+      await db.query(
+        'INSERT INTO users (username, password, email) VALUES ($1, $2, $3)',
+        [req.body.fname + ' ' + req.body.lname, hash.generate(req.body.password), req.body.email],
+      );
+    }
+
+    res.json({
+      ok: ok,
+      errors: errors,
+    });
   });
-}
+};
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -12,43 +12,57 @@
           <div class="row">
             <div class="form-group col-6">
               <label for="fname">First name</label>
-              <input type="text" class="form-control" id="fname" aria-describedby="fname_err" placeholder="First name">
-              <small id="fname_err" class="form-text text-muted"></small>
+              <input type="text" class="form-control" id="fname" v-model="form.fname"
+                aria-describedby="fname_err" placeholder="First name"
+              />
+              <small id="fname_alert" class="form-text">{{alerts.fname}}</small>
             </div>
 
             <div class="form-group col-6">
               <label for="lname">Last name</label>
-              <input type="text" class="form-control" id="lname" aria-describedby="lname_err" placeholder="Last name">
-              <small id="lname_err" class="form-text text-muted"></small>
+              <input type="text" class="form-control" id="lname" v-model="form.lname"
+                aria-describedby="lname_err" placeholder="Last name"
+              />
+              <small id="lname_alert" class="form-text">{{alerts.lname}}</small>
             </div>
           </div>
 
           <div class="form-group">
             <label for="email">Email address</label>
-            <input type="email" class="form-control" id="email" aria-describedby="email_err" placeholder="Email">
-            <small id="email_err" class="form-text text-muted"></small>
+            <input type="email" class="form-control" id="email" v-model="form.email"
+              aria-describedby="email_err" placeholder="Email"
+            />
+            <small id="email_alert" class="form-text">{{alerts.email}}</small>
           </div>
 
           <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" aria-describedby="password_err" placeholder="Password">
-            <small id="password_err" class="form-text text-muted"></small>
+            <input type="password" class="form-control" id="password" v-model="form.password"
+              aria-describedby="password_alert" placeholder="Password"
+            />
+            <small id="password_alert" class="form-text">{{alerts.password}}</small>
           </div>
 
           <div class="form-group">
             <label for="cpassword">Confirm password</label>
-            <input type="password" class="form-control" id="cpassword" aria-describedby="cpassword_err" placeholder="Confirm password">
-            <small id="cpassword_err" class="form-text text-muted"></small>
+            <input type="password" class="form-control" id="cpassword" v-model="form.cpassword"
+              aria-describedby="cpassword_alert" placeholder="Confirm password"
+            />
+            <small id="cpassword_alert" class="form-text">{{alerts.cpassword}}</small>
+          </div>
+
+          <div class="row">
+            <div class="col-6"></div>
+            <div class="col-6">
+              <button type="button" class="create-btn btn btn-success d-none d-xl-block"
+                @click="create">Create Account
+              </button>
+              <button type="button" class="create-btn btn btn-success d-xl-none" @click="create">
+                Create
+              </button>
+            </div>
           </div>
         </form>
-
-        <div class="row">
-          <div class="col-6"></div>
-          <div class="col-6">
-            <button class="create-btn btn btn-success d-none d-xl-block">Create Account</button>
-            <button class="create-btn btn btn-success d-xl-none">Create</button>
-          </div>
-        </div>
       </div>
     </div>
   </div>
@@ -57,12 +71,67 @@
 <script>
 export default {
   name: 'register',
+  data: () => ({
+    form: {
+      fname: '',
+      lname: '',
+      email: '',
+      password: '',
+      cpassword: '',
+    },
+    alerts: {
+      fname: '',
+      lname: '',
+      email: '',
+      password: '',
+      cpassword: '',
+    },
+  }),
+  methods: {
+    create() {
+      fetch('/api/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.form),
+      })
+        .then(res => res.json())
+        .then((res) => {
+          this.setAlerts(res);
+        });
+    },
+    setAlerts(res) {
+      this.alerts = {
+        fname: '',
+        lname: '',
+        email: '',
+        password: '',
+        cpassword: '',
+      };
+
+      if (res.ok) {
+        this.form = {
+          fname: '',
+          lname: '',
+          email: '',
+          password: '',
+          cpassword: '',
+        };
+        return;
+      }
+
+      res.errors.forEach((alert) => {
+        this.alerts[alert.target] = alert.message;
+      });
+    },
+  },
 };
 </script>
 
 <style scoped>
 #title {
-  text-align: center;
+  text-align: center
 }
 
 h1 {
@@ -89,6 +158,11 @@ label {
   color: #263238;
   margin-bottom: 2px;
   letter-spacing: 0.5px;
+}
+
+small {
+  color: #f44336;
+  font-size: 14px;
 }
 
 .create-btn {
